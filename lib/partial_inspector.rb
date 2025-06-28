0d750614f729239095fd06c9f6f5f04e9e5e01fb
SalihaShahid <salihashahid1102@gmail.com>
2025-06-28 23:38:54 +0500

    initial commit

---
+++ lib/partial_inspector.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "partial_inspector/version"
+
+module PartialInspector
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/partial_inspector/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module PartialInspector
+  VERSION = "0.1.0"
+end
