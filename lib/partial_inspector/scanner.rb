c8acd3ec8600dcc2ac266ca52b6359d974860bdb
SalihaShahid <salihashahid1102@gmail.com>
2025-07-03 02:03:34 +0500

    Refactor logic for base scanner and check_partial_exists method

--- lib/partial_inspector/scanner.rb
+++ lib/partial_inspector/scanner.rb
@@ -5,15 +5,17 @@ module PartialInspector
     include PartialInspector::Utils
 
     def inspect_files_rendering_partial(partial_path)
-      puts "CONTROLLER"
-      puts inspect_controller_files_rendering_partial(partial_path)
-      puts "controller count=#{inspect_controller_files_rendering_partial(partial_path).size}"
-      puts "VIEW"
-      puts inspect_view_files_rendering_partial(partial_path)
-      puts "view count=#{inspect_view_files_rendering_partial(partial_path).size}"
-      puts "ADMIN"
-      puts inspect_admin_files_rendering_partial(partial_path)
-      puts "admin count=#{inspect_admin_files_rendering_partial(partial_path).size}"
+      # puts "CONTROLLER"
+      # puts inspect_controller_files_rendering_partial(partial_path)
+      # puts "controller count=#{inspect_controller_files_rendering_partial(partial_path).size}"
+      # puts "VIEW"
+      # puts inspect_view_files_rendering_partial(partial_path)
+      # puts "view count=#{inspect_view_files_rendering_partial(partial_path).size}"
+      # puts "ADMIN"
+      # puts inspect_admin_files_rendering_partial(partial_path)
+      # puts "admin count=#{inspect_admin_files_rendering_partial(partial_path).size}"
+      puts base_scanner(partial_path)
+      puts "count: #{base_scanner(partial_path).size}"
     end
 
     private
@@ -22,7 +24,7 @@ module PartialInspector
       Dir.glob("#{BASE_DIR}/app/controllers/**/*.rb").each do |file|
         file_content = File.readlines(file)
         file_content.each_with_index do |line, index|
-          if line.include?('render') && line.include?('partial:') && check_partial_exists(line, partial_path, "controller")
+          if line.include?('render') && check_partial_exists(line, partial_path)
             lines << "#{file}:#{index + 1}: #{line.strip}"
           end
         end
@@ -36,7 +38,7 @@ module PartialInspector
       Dir.glob("#{BASE_DIR}/app/views/**/*.*.erb").each do |file|
         file_content = File.readlines(file)
         file_content.each_with_index do |line, index|
-          if line.include?('render') && check_partial_exists(line, partial_path, "view")
+          if line.include?('render') && check_partial_exists(line, partial_path)
             lines << "#{file}:#{index + 1}: #{line.strip}"
           end
         end
@@ -50,7 +52,7 @@ module PartialInspector
       Dir.glob("#{BASE_DIR}/app/admin/**/*.rb").each do |file|
         file_content = File.readlines(file)
         file_content.each_with_index do |line, index|
-          if line.include?('render') && line.include?('partial:') && check_partial_exists(line, partial_path, "admin")
+          if line.include?('render') && check_partial_exists(line, partial_path)
             lines << "#{file}:#{index + 1}: #{line.strip}"
           end
         end
--- lib/partial_inspector/utils.rb
+++ lib/partial_inspector/utils.rb
@@ -3,16 +3,20 @@ module PartialInspector
     BASE_DIR = "."
 
     private
-    def base_scanner(file_type)
+    def base_scanner(partial_path)
       lines = []
-      Dir.glob("#{BASE_DIR}/app/#{file_type.pluralize}/**/*.rb").each do |file|
+
+      files = Dir.glob("./app/**/*.{rb,html.erb,js.erb,turbo_stream.erb}")
+      files.each do |file|
         file_content = File.readlines(file)
         file_content.each_with_index do |line, index|
-          if line.include?('render') && line.include?('partial:') && check_partial_exists(line, partial_path, file_type)
+          if (line.include?('render') || line.include?('partial:')) && check_partial_exists(line, partial_path)
             lines << "#{file}:#{index + 1}: #{line.strip}"
           end
         end
       end
+
+      lines
     end
 
     def extract_partial_file_name(partial_path)
@@ -27,18 +31,16 @@ module PartialInspector
       partial_name
     end
 
-    def check_partial_exists(line, partial, file_type)
+    def check_partial_exists(line, partial)
       partial_path = ""
       line_content = line.split(' ')
       line_content.each_with_index do |value, index|
-        if file_type == "view"
-          if line.include?("render partial:") && value == "render"
-            partial_path = line_content[index + 2]
-            break
-          elsif value == "render"
-            partial_path = line_content[index + 1]
-            break
-          end
+        if line.include?("render partial:") && value == "render"
+          partial_path = line_content[index + 2]
+          break
+        elsif value == "render"
+          partial_path = line_content[index + 1]
+          break
         elsif value == "partial:"
           partial_path = line_content[index + 1]
           break
